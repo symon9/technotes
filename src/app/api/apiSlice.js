42f003b07938587b0f4a406f75575db1ca694839
Simon Emmanuel <simonemmanuel743@gmail.com>
2024-09-19 13:23:36 +0100

    update backend base url

--- src/app/api/apiSlice.js
+++ src/app/api/apiSlice.js
@@ -2,7 +2,7 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 import { setCredentials } from "../../features/auth/authSlice";
 
 const baseQuery = fetchBaseQuery({
-  baseUrl: "https://technotes-v1-api.onrender.com",
+  baseUrl: "https://technotes-api-53o7.onrender.com",
   credentials: "include",
   prepareHeaders: (headers, { getState }) => {
     const token = getState().auth.token;
